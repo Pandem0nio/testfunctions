97e93324a14f292e3f5fb8a4593cace7e26cbde3
Pandem0nio <jtlabs@outlook.it>
2017-03-28 16:51:27 +0200

    sgfshgfzg

--- AzureInsightsConsumer/run.js
+++ AzureInsightsConsumer/run.js
@@ -1,8 +1,11 @@
 module.exports = function (context) {
     context.log(context.req.query.name);
-
-    context.res = { status: 200, body: 'hello ' + context.req.query.name };
+    var newstr = reverse(context.req.query.name);
+    context.res = { status: 200, body: 'reverse string = ' + newstr };
 
     context.done();
-    
+
+    function reverse(s) {
+        return s.split("").reverse().join("");
+    }
 };
