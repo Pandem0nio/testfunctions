d69e2dd72a6deaf6485dfa4ef1bf267fcb7ebd17
Pandem0nio <jtlabs@outlook.it>
2017-03-28 16:13:39 +0200

    ytgyitvu

--- AzureInsightsConsumer/run.js
+++ AzureInsightsConsumer/run.js
@@ -1,6 +1,6 @@
 module.exports = function (context) {
     context.log(context.req.query.name);
 
-    context.res = { status: 202, body: 'hello ' + context.req.query.name };
+    context.res = { status: 202, body: 'hello' };
     
 };
